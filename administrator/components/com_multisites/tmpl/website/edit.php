cb397c5b334020464df2ce79def70f70b7286386
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2023-03-19 14:50:51 +0100

    Move URL field to the top

--- administrator/components/com_multisites/tmpl/website/edit.php
+++ administrator/components/com_multisites/tmpl/website/edit.php
@@ -48,8 +48,8 @@ $this->ignore_fieldsets = ['general'];
                     <?php echo $this->form->renderFieldset('general'); ?>
                 </fieldset>
                 <fieldset class="form-vertical">
-                    <?php echo $this->form->renderField('title_native'); ?>
                     <?php echo $this->form->renderField('baseurl'); ?>
+                    <?php echo $this->form->renderField('title_native'); ?>
                 </fieldset>
             </div>
             <div class="col-lg-3 ms-lg-auto">
