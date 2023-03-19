46835d9a3e1a84c0884253eb8180db25afbc53b4
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2023-03-19 15:27:04 +0100

    Add default toggle for groups

--- administrator/components/com_multisites/src/Model/GroupModel.php
+++ administrator/components/com_multisites/src/Model/GroupModel.php
@@ -166,6 +166,62 @@ class GroupModel extends AdminModel
 
 				return $data;
 		}
+		/**
+		 * Method to change the default state of one item.
+		 *
+		 * @param   array    $pk     A list of the primary keys to change.
+		 * @param   integer  $value  The value of the home state.
+		 *
+		 * @return  boolean  True on success.
+		 *
+		 * @since  __DEPLOY_VERSION__
+		 */
+		public function setDefault($pk, $value = 1)
+		{
+			$table = $this->getTable();
+	
+			if ($table->load($pk)) {
+				if ($table->state !== 1) {
+					$this->setError(Text::_('COM_MULTISITE_DEFAULT_ITEM_HAS_TO_BE_PUBLISHED'));
+	
+					return false;
+				}
+			}
+	
+			if (empty($table->id) || !$this->canEditState($table)) {
+				Log::add(Text::_('JLIB_APPLICATION_ERROR_EDITSTATE_NOT_PERMITTED'), Log::WARNING, 'jerror');
+	
+				return false;
+			}
+	
+			$date = Factory::getDate()->toSql();
+	
+			if ($value) {
+				// Unset other default item
+				if (
+					$table->load(
+						[
+						'default' => '1',
+						]
+					)
+				) {
+					$table->default  = 0;
+					$table->modified = $date;
+					$table->store();
+				}
+			}
+	
+			if ($table->load($pk)) {
+				$table->modified = $date;
+				$table->default  = $value;
+				$table->store();
+			}
+	
+			// Clean the cache
+			$this->cleanCache();
+	
+			return true;
+		}
 
 		/**
 		 * Method to validate the form data.
